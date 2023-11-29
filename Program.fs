e8fb5f1f0d658d748f41bf2329abe2bbf4e66eba
khushpreetKaurgill <KhushpreetKaurhwu@triosstudent.com>
2023-11-29 09:09:11 -0500

    v

---
+++ Program.fs
@@ -0,0 +1,24 @@
+﻿type Genre =
+    | Horror
+    | Drama
+    | Thriller
+    | Comedy
+    | Fantasy
+    | Sport
+
+type Director = {
+    Name: string
+    Movies: int
+}
+
+type Movie = {
+    Name: string
+    RunLength: int
+    Genre: Genre
+    Director: Director
+    IMDBRating: float
+}
+
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from Khush#"
